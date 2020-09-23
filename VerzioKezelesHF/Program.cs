0ec88dc2e351ff6f9a6cfeb6ad32468a6ab2d016
Keanu Reeves <tzas96@gmail.com>
2020-09-23 13:03:10 +0200

    Add project files.

---
+++ VerzioKezelesHF/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerzioKezelesHF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = 10;
+            List<int> szamok = new List<int>();
+            string[] st;
+            Console.WriteLine("Adjon meg 10 számot (szóközzel elválasztva): ");
+            string szam = Console.ReadLine();
+            for (int i = 0; i < N; i++)
+            {
+                st = szam.Split(' ');
+                if (st.Length==N)
+                {
+                    szamok.Add(Convert.ToInt32(st[i]));
+                }
+                
+            }
+            foreach (var item in szamok)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
